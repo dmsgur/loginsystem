5a1c1814b972fa4b938505ab5e6d206e2ff85393
kongs7 <kongs7@DESKTOP-91R30H9>
2025-01-02 17:12:11 +0900

    1

--- menu.js
+++ menu.js
@@ -10,14 +10,6 @@ menu0 = new Menu("로그인시스템")
 menu0.url = "https://dmsgur.github.io/loginsystem/"
 menu0.tips = "스프링 mybatis를 활용한 간단한 로그인 시스템"
 
-menu1 = new Menu("CNN AND Crawling");
-menu1.url="https://dmsgur.github.io/classification_model/"
-menu1.tips="네이버,구글 이미지 크롤링 및 컨볼루션 적용한 모델";
-
-menu2 = new Menu("RNN AND LSTM encryto money")
-menu2.url = "https://dmsgur.github.io/RNN_Encrypto/"
-menu2.tips = "가상화폐 분석을 이용한 미래 가격 측정"
 
 menu_sets.push(menu0)
-menu_sets.push(menu1)
-menu_sets.push(menu2)
+
