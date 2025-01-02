ff7188ff0ac5c9cec1a181abf2f1b5a77849a8ea
kongs7 <kongs7@DESKTOP-91R30H9>
2025-01-02 17:52:18 +0900

    2

--- menu.js
+++ menu.js
@@ -8,7 +8,7 @@ class Menu{
 }
 menu0 = new Menu("개발환경구축")//1. 개발목적/언어-선정/요구사항명세/분석 2. 구현도구/라이센스 3. 테스트도구(junit,mockobj)  4. 형상관리도구  5. 빌드도구
 menu0.url = "https://dmsgur.github.io/loginsystem/"
-menu0.tips = "스프링 mybatis를 활용한 간단한 로그인 시스템"
+menu0.tips = ["1.개발목적","2.개발언어선정","3.요구사항명세","4.구현도구","5.테스트도구","6.형상관리도구","7.빌드도구"]
 menu1 = new Menu("개발환경구축")
 menu1.url = "https://dmsgur.github.io/loginsystem/"
 menu1.tips = "스프링 mybatis를 활용한 간단한 로그인 시스템"
