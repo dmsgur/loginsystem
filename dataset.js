c7f0477366b4cc5f19f12cfaec04cee3ac57914f
kongs7 <kongs7@DESKTOP-91R30H9>
2025-01-03 15:17:06 +0900

    1

--- dataset.js
+++ dataset.js
@@ -28,32 +28,32 @@ d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 설명
 d1.set_content("개발언어선정을 작성합니다.")
 d1.set_imgtitle("이미지 타이틀")//이미지타이틀
 d1.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d1.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_content("요구사항명세를 작성합니다.")
 d1.set_imgtitle("이미지 타이틀")//이미지타이틀
 d1.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d1.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_content("구현도구를 작성합니다.")
 d1.set_imgtitle("이미지 타이틀")//이미지타이틀
 d1.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d1.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_content("테스트도구를 작성합니다.")
 d1.set_imgtitle("이미지 타이틀")//이미지타이틀
 d1.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d1.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_content("형상관리도구를 작성합니다.")
 d1.set_imgtitle("이미지 타이틀")//이미지타이틀
 d1.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d1.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_content("빌드도구를 작성합니다.")
 d1.set_imgtitle("이미지 타이틀")//이미지타이틀
 d1.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d1.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d1.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d1)
@@ -64,17 +64,17 @@ let d2 = new DataSet("공통모듈구현")//메인타이틀
 d2.set_content("공통모듈구현을 작성합니다.")//서브 타이틀
 d2.set_imgtitle("이미지 타이틀")//이미지타이틀
 d2.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d2.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d2.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d2.set_content("테스트케이스를 작성합니다.")
 d2.set_imgtitle("이미지 타이틀")//이미지타이틀
 d2.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d2.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d2.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d2.set_content("단위테스트를 작성합니다.")
 d2.set_imgtitle("이미지 타이틀")//이미지타이틀
 d2.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d2.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d2.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d1.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d2)
@@ -84,32 +84,32 @@ let d3 = new DataSet("서버프로그램구현")//메인타이틀
 d3.set_content("회원가입구현")//서브타이틀
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d3.set_content("회원로그인구현")
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d3.set_content("회원리스트연동")
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d3.set_content("테스트케이스 작성")
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d3.set_content("목오브젝트생성 및 테스트 구현")
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d3.set_content("결과보고서 작성")
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d3.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d3)
@@ -120,11 +120,11 @@ let d4 = new DataSet("배치프로그램구현")//메인타이틀
 d4.set_content("배치스케줄구현")//서브타이틀
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 
 d4.set_content("배치프로그램테스트 구현")
 d3.set_imgtitle("이미지 타이틀")//이미지타이틀
 d3.set_img("1S0rv5tVKoKnAqKRLrW7huVwCqFct3AnY")//스냅샷
-d3.set_log("이미지에 대한 간략한 설명 서술")//이미지 서술
+d3.set_imglog("이미지에 대한 간략한 설명 서술")//이미지 서술
 d3.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d4)
